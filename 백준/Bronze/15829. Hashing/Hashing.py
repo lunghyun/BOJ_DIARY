7d0716824668e1061d563f88dab5de36450a6ba0
lunghyun <mrpn604@gmail.com>
2024-04-23 00:50:58 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+s = input().strip()
+
+# i * 31 mod 1234567891
+result = 0
+for i in range(n):
+    result += (ord(s[i]) - 96) * 31**i
+print(result)
